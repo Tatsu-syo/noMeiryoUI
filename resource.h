22dddd8bac71d5f4e38f0f5d947ac7b0fdb028f1
Tatsu <Tatsu@2be31de6-1b40-1d45-9c1f-7f81fcd71ed7>
2012-11-24 08:10:53 +0000

    MeiryoUIも大っきらい!!ソースファイル追加。
    git-svn-id: file:///H/repositry/noTahoma/noMeiryoUI/trunk@21 2be31de6-1b40-1d45-9c1f-7f81fcd71ed7

---
+++ resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by noMeiryoUI.rc
+//
+#define IDC_MYICON                      2
+#define ID_SEL_ALL                      3
+#define ID_SEL_TITLE                    4
+#define ID_SEL_ICON                     5
+#define ID_SEL_PALETTE                  6
+#define ID_SEL_HINT                     7
+#define ID_SEL_MESSAGE                  8
+#define ID_SEL_MENU                     9
+#define ID_SET_ALL                      11
+#define IDD_NOMEIRYOUI_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDD_MAIN_DIALOG                 103
+#define IDM_ABOUT                       104
+#define IDI_NOMEIRYOUI                  107
+#define IDI_SMALL                       108
+#define IDC_NOMEIRYOUI                  109
+#define IDR_MAINFRAME                   128
+#define IDR_RT_MANIFEST1                129
+#define IDR_RT_MANIFEST                 131
+#define IDC_EDIT_ALL                    1000
+#define IDC_EDIT_TITLE                  1001
+#define IDC_EDIT_ICON                   1002
+#define IDC_EDIT_PALETTE                1003
+#define IDC_EDIT_HINT                   1004
+#define IDC_EDIT_MESSAGE                1005
+#define IDC_EDIT_MENU                   1006
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
