556680e55d99de7eecdb9715ecf998131842e85d
Tatsu <Tatsu@2be31de6-1b40-1d45-9c1f-7f81fcd71ed7>
2012-11-24 08:10:53 +0000

    MeiryoUIも大っきらい!!ソースファイル追加。
    git-svn-id: file:///H/repositry/noTahoma/noMeiryoUI/trunk@23 2be31de6-1b40-1d45-9c1f-7f81fcd71ed7

---
+++ stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーから使用されていない部分を除外します。
+// Windows ヘッダー ファイル:
+#include <windows.h>
+
+// C ランタイム ヘッダー ファイル
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// TODO: プログラムに必要な追加ヘッダーをここで参照してください。
