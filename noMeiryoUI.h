9a0575f5aa8ebe186b21adc78d8aa9bfb762321f
Tatsu <Tatsu@2be31de6-1b40-1d45-9c1f-7f81fcd71ed7>
2012-11-24 08:10:52 +0000

    MeiryoUIも大っきらい!!ソースファイル追加。
    git-svn-id: file:///H/repositry/noTahoma/noMeiryoUI/trunk@14 2be31de6-1b40-1d45-9c1f-7f81fcd71ed7

---
+++ noMeiryoUI.h
@@ -0,0 +1,57 @@
+
+#ifndef MYAPPLI_H
+#define MYAPPLI_H
+
+#include "DialogAppliBase.h"
+
+class NoMeiryoUI : public DialogAppliBase {
+private:
+
+	enum fontType {
+		all,
+		title,
+		icon,
+		palette,
+		hint,
+		message,
+		menu
+	};
+
+	tstring allFontName;
+	tstring titleFontName;
+	tstring iconFontName;
+	tstring paletteFontName;
+	tstring hintFontName;
+	tstring messageFontName;
+	tstring menuFontName;
+
+	NONCLIENTMETRICS metrics;
+	LOGFONT iconFont;
+
+	NONCLIENTMETRICS metricsAll;
+	LOGFONT iconFontAll;
+
+	void selectFont(enum fontType type);
+	void OnBnClickedOk();
+	void OnBnClickedAll();
+
+protected:
+	INT_PTR OnCommand(WPARAM wParam);
+
+public:
+	BaseDialog *createBaseDialog();
+	int OnAppliStart(TCHAR *lpCmdLine);
+	int OnWindowShow();
+	int OnAppliEnd();
+	INT_PTR OnInitDialog();
+	void UpdateData(bool toObj);
+
+};
+
+#include	<windows.h>
+#include	<sddl.h>
+#include	<tchar.h>
+
+TCHAR *getSID(void);
+
+#endif
