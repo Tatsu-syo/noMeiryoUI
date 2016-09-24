bf6dce5fe6913193d1775f36fb1d4bd544ff48d8
Tatsu <Tatsu@2be31de6-1b40-1d45-9c1f-7f81fcd71ed7>
2016-09-24 08:01:38 +0000

    ・言語リソースを読み込めなかった場合に英語表示を行うようにした。
    git-svn-id: file:///H/repositry/noTahoma/noMeiryoUI/trunk@131 2be31de6-1b40-1d45-9c1f-7f81fcd71ed7

--- program/util.cpp
+++ program/util.cpp
@@ -92,16 +92,22 @@ double getFontPoint(LOGFONT *font, HWND hWnd)
  *
  * @param file リソースファイル名
  * @param key キー名
+ * @param 読み込めなかった時の文字列
  */
-void readResourceItem(TCHAR *file, TCHAR *key)
+void readResourceItem(TCHAR *file, TCHAR *key, TCHAR *fallback)
 {
 	TCHAR buf[255];
+	int len;
 
 	// INIファイルを読み込む。Unicode版のAPIでもファイルが非Unicodeの場合は
 	// 各言語の文字コードのファイルとして読んでくれる。
-	GetPrivateProfileString(_T("RESOURCE"), key, _T(""), buf, 255, file);
+	len = GetPrivateProfileString(_T("RESOURCE"), key, _T(""), buf, 255, file);
+	if (len > 0) {
+		langResource.push_back(buf);
+	} else {
+		langResource.push_back(fallback);
+	}
 
-	langResource.push_back(buf);
 }
 
 /**
@@ -111,78 +117,218 @@ void readResourceItem(TCHAR *file, TCHAR *key)
  */
 void readResourceFile(TCHAR *file)
 {
-	readResourceItem(file, _T("FONT_FACE"));
-	readResourceItem(file, _T("TITLE"));
-	readResourceItem(file, _T("MENU_FILE"));
-	readResourceItem(file, _T("MENU_FILE_LOAD"));
-	readResourceItem(file, _T("MENU_FILE_SAVE"));
-	readResourceItem(file, _T("MENU_FILE_SET_QUIT"));
-	readResourceItem(file, _T("MENU_FILE_QUIT"));
-	readResourceItem(file, _T("MENU_PRESET"));
-	readResourceItem(file, _T("MENU_PRESET_8"));
-	readResourceItem(file, _T("MENU_PRESET_10"));
-	readResourceItem(file, _T("MENU_TOOLS"));
-	readResourceItem(file, _T("MENU_TOOLS_THREAD"));
-	readResourceItem(file, _T("MENU_TOOLS_SEVEN"));
-	readResourceItem(file, _T("MENU_HELP"));
-	readResourceItem(file, _T("MENU_HELP_HELP"));
-	readResourceItem(file, _T("MENU_HELP_ABOUT"));
-	readResourceItem(file, _T("DLG_ALL_FONT"));
-	readResourceItem(file, _T("DLG_TITLE_BAR"));
-	readResourceItem(file, _T("DLG_ICON"));
-	readResourceItem(file, _T("DLG_PALETTE"));
-	readResourceItem(file, _T("DLG_HINT"));
-	readResourceItem(file, _T("DLG_MESSAGE"));
-	readResourceItem(file, _T("DLG_MENU"));
-	readResourceItem(file, _T("DLG_SELECT"));
-	readResourceItem(file, _T("DLG_SET_ALL"));
-	readResourceItem(file, _T("DLG_SET"));
-	readResourceItem(file, _T("DLG_EXIT"));
-	readResourceItem(file, _T("DLG_FONT_SEL"));
-	readResourceItem(file, _T("DLG_FONT_NAME"));
-	readResourceItem(file, _T("DLG_STYLE"));
-	readResourceItem(file, _T("DLG_SIZE"));
-	readResourceItem(file, _T("DLG_UNDERLINE"));
-	readResourceItem(file, _T("DLG_STRIKE"));
-	readResourceItem(file, _T("DLG_CHARSET"));
-	readResourceItem(file, _T("DLG_OK"));
-	readResourceItem(file, _T("DLG_CANCEL"));
+	readResourceItem(file, _T("FONT_FACE"),
+		_T("System")
+	);
+	readResourceItem(file, _T("TITLE"),
+		_T("No!! Meiryo UI")
+	);
+	readResourceItem(file, _T("MENU_FILE"),
+		_T("&File")
+	);
+	readResourceItem(file, _T("MENU_FILE_LOAD"),
+		_T("L&oad font settings...")
+	);
+	readResourceItem(file, _T("MENU_FILE_SAVE"),
+		_T("S&ave font settings...")
+	);
+	readResourceItem(file, _T("MENU_FILE_SET_QUIT"),
+		_T("&Set system fonts and quit")
+	);
+	readResourceItem(file, _T("MENU_FILE_QUIT"),
+		_T("E&xit")
+	);
+	readResourceItem(file, _T("MENU_PRESET"),
+		_T("&Preset")
+	);
+	readResourceItem(file, _T("MENU_PRESET_8"),
+		_T("Windows 8/8.1")
+	);
+	readResourceItem(file, _T("MENU_PRESET_10"),
+		_T("Windows 10")
+	);
+	readResourceItem(file, _T("MENU_TOOLS"),
+		_T("&Tools")
+	);
+	readResourceItem(file, _T("MENU_TOOLS_THREAD"),
+		_T("Set system fonts in separated thread")
+	);
+	readResourceItem(file, _T("MENU_TOOLS_SEVEN"),
+		_T("Calculate font size same as Windows 7")
+	);
+	readResourceItem(file, _T("MENU_HELP"),
+		_T("&Help")
+	);
+	readResourceItem(file, _T("MENU_HELP_HELP"),
+		_T("&Help")
+	);
+	readResourceItem(file, _T("MENU_HELP_ABOUT"),
+		_T("&About No!! Meiryo UI")
+	);
+	readResourceItem(file, _T("DLG_ALL_FONT"),
+		_T("All fonts")
+	);
+	readResourceItem(file, _T("DLG_TITLE_BAR"),
+		_T("Title bar")
+	);
+	readResourceItem(file, _T("DLG_ICON"),
+		_T("Icon")
+	);
+	readResourceItem(file, _T("DLG_PALETTE"),
+		_T("Palette title")
+	);
+	readResourceItem(file, _T("DLG_HINT"),
+		_T("Hint")
+	);
+	readResourceItem(file, _T("DLG_MESSAGE"),
+		_T("Message box")
+	);
+	readResourceItem(file, _T("DLG_MENU"),
+		_T("Menu")
+	);
+	readResourceItem(file, _T("DLG_SELECT"),
+		_T("Select...")
+	);
+	readResourceItem(file, _T("DLG_SET_ALL"),
+		_T("Set all")
+	);
+	readResourceItem(file, _T("DLG_SET"),
+		_T("Set")
+	);
+	readResourceItem(file, _T("DLG_EXIT"),
+		_T("Exit")
+	);
+	readResourceItem(file, _T("DLG_FONT_SEL"),
+		_T("Font Selection")
+	);
+	readResourceItem(file, _T("DLG_FONT_NAME"),
+		_T("&Font name")
+	);
+	readResourceItem(file, _T("DLG_STYLE"),
+		_T("St&yle")
+	);
+	readResourceItem(file, _T("DLG_SIZE"),
+		_T("&Size")
+	);
+	readResourceItem(file, _T("DLG_UNDERLINE"),
+		_T("&Under line")
+	);
+	readResourceItem(file, _T("DLG_STRIKE"),
+		_T("S&trikethrough")
+	);
+	readResourceItem(file, _T("DLG_CHARSET"),
+		_T("&Character set")
+	);
+	readResourceItem(file, _T("DLG_OK"),
+		_T("OK")
+	);
+	readResourceItem(file, _T("DLG_CANCEL"),
+		_T("Cancel")
+	);
 
-	readResourceItem(file, _T("DLG_CHARSET_ANSI"));
-	readResourceItem(file, _T("DLG_CHARSET_BALTIC"));
-	readResourceItem(file, _T("DLG_CHARSET_BIG5"));
-	readResourceItem(file, _T("DLG_CHARSET_DEFAULT"));
-	readResourceItem(file, _T("DLG_CHARSET_EASTEUROPE"));
-	readResourceItem(file, _T("DLG_CHARSET_GB2312"));
-	readResourceItem(file, _T("DLG_CHARSET_GREEK"));
-	readResourceItem(file, _T("DLG_CHARSET_HANGUL"));
-	readResourceItem(file, _T("DLG_CHARSET_MAC"));
-	readResourceItem(file, _T("DLG_CHARSET_OEM"));
-	readResourceItem(file, _T("DLG_CHARSET_RUSSIAN"));
-	readResourceItem(file, _T("DLG_CHARSET_SHIFTJIS"));
-	readResourceItem(file, _T("DLG_CHARSET_SYMBOL"));
-	readResourceItem(file, _T("DLG_CHARSET_TURKISH"));
-	readResourceItem(file, _T("DLG_CHARSET_VIETNAMESE"));
-	readResourceItem(file, _T("DLG_CHARSET_JOHAB"));
-	readResourceItem(file, _T("DLG_CHARSET_ARABIC"));
-	readResourceItem(file, _T("DLG_CHARSET_HEBREW"));
-	readResourceItem(file, _T("DLG_CHARSET_THAI"));
+	readResourceItem(file, _T("DLG_CHARSET_ANSI"),
+		_T("Ansi")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_BALTIC"),
+		_T("Baltic")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_BIG5"),
+		_T("Traditional Chinese")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_DEFAULT"),
+		_T("Default")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_EASTEUROPE"),
+		_T("East Europe")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_GB2312"),
+		_T("Simplified Chinese")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_GREEK"),
+		_T("Greek")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_HANGUL"),
+		_T("Hangul")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_MAC"),
+		_T("Mac")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_OEM"),
+		_T("OEM")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_RUSSIAN"),
+		_T("Russian")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_SHIFTJIS"),
+		_T("Japanese")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_SYMBOL"),
+		_T("Symbol")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_TURKISH"),
+		_T("Turkish")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_VIETNAMESE"),
+		_T("Vietnamese")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_JOHAB"),
+		_T("Johab Korean")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_ARABIC"),
+		_T("Arabic")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_HEBREW"),
+		_T("Hebrew")
+	);
+	readResourceItem(file, _T("DLG_CHARSET_THAI"),
+		_T("Thai")
+	);
 
-	readResourceItem(file, _T("DLG_STYLE_NORMAL"));
-	readResourceItem(file, _T("DLG_STYLE_ITALIC"));
-	readResourceItem(file, _T("DLG_STYLE_BOLD"));
-	readResourceItem(file, _T("DLG_STYLE_BOLD_ITALIC"));
+	readResourceItem(file, _T("DLG_STYLE_NORMAL"),
+		_T("Normal")
+	);
+	readResourceItem(file, _T("DLG_STYLE_ITALIC"),
+		_T("Italic")
+	);
+	readResourceItem(file, _T("DLG_STYLE_BOLD"),
+		_T("Bold")
+	);
+	readResourceItem(file, _T("DLG_STYLE_BOLD_ITALIC"),
+		_T("Bold Italic")
+	);
 
-	readResourceItem(file, _T("MSG_SETTING_FILE"));
-	readResourceItem(file, _T("MSG_ALL_FILE"));
-	readResourceItem(file, _T("MSG_LOAD_FAIL"));
-	readResourceItem(file, _T("MSG_SAVE_FAIL"));
-	readResourceItem(file, _T("MSG_ERROR"));
-	readResourceItem(file, _T("MSG_ABOUT"));
-	readResourceItem(file, _T("MSG_NO_FONT"));
-	readResourceItem(file, _T("MSG_NO_STYLE"));
-	readResourceItem(file, _T("MSG_NO_SIZE"));
-	readResourceItem(file, _T("MSG_NO_CHARSET"));
-	readResourceItem(file, _T("MSG_TRANSLATE"));
+	readResourceItem(file, _T("MSG_SETTING_FILE"),
+		_T("Font settings file(*.ini)")
+	);
+	readResourceItem(file, _T("MSG_ALL_FILE"),
+		_T("All file(*.*)")
+	);
+	readResourceItem(file, _T("MSG_LOAD_FAIL"),
+		_T("Font settings file load failed.")
+	);
+	readResourceItem(file, _T("MSG_SAVE_FAIL"),
+		_T("Font settings file save failed.")
+	);
+	readResourceItem(file, _T("MSG_ERROR"),
+		_T("Error")
+	);
+	readResourceItem(file, _T("MSG_ABOUT"),
+		_T("About No!! Meiryo UI")
+	);
+	readResourceItem(file, _T("MSG_NO_FONT"),
+		_T("Please select font.")
+	);
+	readResourceItem(file, _T("MSG_NO_STYLE"),
+		_T("Please select style.")
+	);
+	readResourceItem(file, _T("MSG_NO_SIZE"),
+		_T("Please select size.")
+	);
+	readResourceItem(file, _T("MSG_NO_CHARSET"),
+		_T("Please select charset.")
+	);
+	readResourceItem(file, _T("MSG_TRANSLATE"),
+		_T("Tatsuhiko Shoji(Tatsu)")
+	);
 }
 
