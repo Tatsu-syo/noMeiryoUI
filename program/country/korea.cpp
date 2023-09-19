a50c447e2688702b26121efc00e00b0980d411f1
Tatsuhiko Syoji <rxk16664@nifty.ne.jp>
2023-09-19 22:58:41 +0900

    クレジット追加。

--- program/country/korea.cpp
+++ program/country/korea.cpp
@@ -1,4 +1,8 @@
-﻿#include <string.h>
+﻿/*
+noMeiryoUI (C) 2005,2012-2023 Tatsuhiko Shoji
+The sources for noMeiryoUI are distributed under the MIT open source license
+*/
+#include <string.h>
 #include <tchar.h>
 #include <tstring.h>
 
--- program/country/korea.h
+++ program/country/korea.h
@@ -1,3 +1,7 @@
+/*
+noMeiryoUI (C) 2005,2012-2023 Tatsuhiko Shoji
+The sources for noMeiryoUI are distributed under the MIT open source license
+*/
 namespace korea {
 
 	void getKoreanFontName(TCHAR *dispBuf);
