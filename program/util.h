775f66ea7b68a08f568e6c57b4d6329aaf4992f2
Tatsuhiko Syoji <rxk16664@nifty.ne.jp>
2024-02-15 23:58:20 +0900

    Change to compile Visual Studio 2008.

--- program/util.h
+++ program/util.h
@@ -97,6 +97,12 @@ size_t utf8toUtf16(tstring& dst, const char* src);
 int getSystemDPI(void);
 double getFontPointDouble(LOGFONT* font, HWND hWnd);
 
+#if _MSC_VER < 1600
+
+double round(double x);
+
+#endif
+
 /**
  * プリセット設定
  *
