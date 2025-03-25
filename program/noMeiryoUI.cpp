4e4cb7a2e781bfcf9551723ca61e0bdc8f1f8972
Tatsuhiko Shoji <rxk16664@nifty.ne.jp>
2025-03-25 20:58:06 +0900

    Windows 11 Update Version
    Add 2025 Update detection logic.

--- program/noMeiryoUI.cpp
+++ program/noMeiryoUI.cpp
@@ -2697,11 +2697,11 @@ void NoMeiryoUI::getWin10Ver(TCHAR *buf, DWORD major, DWORD minor)
 			break;
 		case 11:
 			_tcscpy_s(calledVer, _T("11"));
-			if (buildNumber > 27999) {
+			if (buildNumber > 27000) {
 				_tcscat_s(calledVer, _T(" Insider"));
-			} else if (buildNumber > 27000) {
+			} else if (buildNumber >= 26200) {
 				_tcscat_s(calledVer, _T(" 2025 Update"));
-			} else if (buildNumber > 26000) {
+			} else if (buildNumber >= 26000) {
 				_tcscat_s(calledVer, _T(" 2024 Update"));
 			} else if (buildNumber == 22631) {
 				_tcscat_s(calledVer, _T(" 2023 Update"));
