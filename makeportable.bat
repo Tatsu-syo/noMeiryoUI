69979ea5a2061c601dd06fcaa13115cffb2357b0
Tatsuhiko Shoji <rxk16664@nifty.ne.jp>
2025-09-07 20:03:56 +0900

    Add Licence Message
    - Add Licence Message

--- makeportable.bat
+++ makeportable.bat
@@ -1,8 +1,10 @@
+REM noMeiryoUI (C) 2005,2012-2025 Tatsuhiko Shoji
+REM The sources for noMeiryoUI are distributed under the MIT open source license
 REM Usage:
 REM Copy portable version of noMeiryoUI.exe into files directory.
 REM run batchfile
 
 nmake
 cd files
-7z a ..\noMeiryoUI.zip @filelist.txt
+7z a -mx9 ..\noMeiryoUI.zip @filelist.txt
 cd ..
