e1a861dbdff2d0960b22c774842661fc0e78c556
Tatsuhiko Shoji <rxk16664@nifty.ne.jp>
2026-02-01 10:09:39 +0900

    Fix pathes.

--- packaging/makeportable.bat
+++ packaging/makeportable.bat
@@ -1,10 +1,13 @@
-REM noMeiryoUI (C) 2005,2012-2025 Tatsuhiko Shoji
+REM noMeiryoUI (C) 2005,2012-2026 Tatsuhiko Shoji
 REM The sources for noMeiryoUI are distributed under the MIT open source license
 REM Usage:
 REM Copy portable version of noMeiryoUI.exe into files directory.
 REM run batchfile
 
-nmake
-cd portable
-7z a -mx9 ..\Output\noMeiryoUI.zip @..\filelist.txt
+nmake clean
+del /Q ..\Output\noMeiryoUI.zip
+copy /y ..\program\Release_2012\noMeiryoUI.exe files
+nmake all
+cd files
+7z a -mx9 ..\..\Output\noMeiryoUI.zip @filelist.txt
 cd ..
